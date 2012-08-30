b977d728ef23e8444adf5585ae9a93ad4ae68768
Joshua Albers <joshua.albers@gmail.com>
2012-08-30 17:52:48 -0500

    updated the readme.txt and added the license files

--- src/cc/arduino/btserial/BtSerial.java
+++ src/cc/arduino/btserial/BtSerial.java
@@ -4,20 +4,20 @@
  *
  * ##copyright##
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This file is part of BtSerial.
  * 
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *   BtSerial is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
  * 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA  02111-1307  USA
+ *   BtSerial is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with BtSerial.  If not, see <http://www.gnu.org/licenses/>.
  * 
  * @author		##author##
  * @modified	##date##
--- src/cc/arduino/btserial/ConnectedThread.java
+++ src/cc/arduino/btserial/ConnectedThread.java
@@ -1,6 +1,29 @@
 /**
- * Blah Blah Blah
+ * ##project.name##
+ * ##library.sentence##
+ *
+ * ##copyright##
+ *
+ * This file is part of BtSerial.
+ * 
+ *   BtSerial is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ * 
+ *   BtSerial is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ * 
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with BtSerial.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author		##author##
+ * @modified	##date##
+ * @version		##library.prettyVersion##
  */
+
 
 package cc.arduino.btserial;
 
